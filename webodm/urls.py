9cbdf5b82babd8b701ccdce3d4a895a8585cdb52
nveronica <veronika_nagy@outlook.hu>
2024-04-08 12:11:47 +0200

    turbine planner & fluid modelling

--- webodm/urls.py
+++ webodm/urls.py
@@ -41,6 +41,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     url(r'^', include('app.urls')),
+    #url(r'^tpfm/', include('tpfm.urls')),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^admin/', admin.site.urls),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
