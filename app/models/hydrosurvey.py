26da535a551c5e246ff51836ff7dc22a989c0fb4
Andraspiti <andraspiti@gmail.com>
2024-04-22 14:44:27 +0200

    hydrosurvey form

--- app/models/hydrosurvey.py
+++ app/models/hydrosurvey.py
@@ -4,19 +4,23 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 
+#CARDINAL_DIRECTIONS = ["Észak", "Kelet","Dél", "Nyugat"]
+#CARDINAL_DIRECTIONS_SORTED= sorted([(item, item) for item in CARDINAL_DIRECTIONS])
+CARDINAL_DIRECTIONS_TUPLE = (("É", "Észak"), ("K", "Kelet"), ("D", "Dél"), ("NY", "Nyugat"))
+
 class HydroSurvey(models.Model):
     id = models.BigAutoField(db_column='id', primary_key=True)
-    name = models.CharField(db_column='name', max_length=250)
-    deadline = models.DateTimeField(db_column='deadline')
-    status = models.CharField(db_column='status', max_length=250)
-    water_surface = models.CharField(db_column='water_surface', max_length=250)
-    location = models.CharField(db_column='location', max_length=250)
-    flow_direction = models.CharField(db_column='flow_direction', max_length=250)
-    flow_direction_speed = models.FloatField(db_column='flow_direction_speed', max_length=250)
-    segment_width = models.DecimalField(db_column='segment_width', max_digits=1000, decimal_places=4, blank=True)
-    segment_depth = models.DecimalField(db_column='segment_depth', max_digits=1000, decimal_places=4, blank=True)
-    estimated_waterflow = models.CharField(db_column='estimated_waterflow', max_length=250)
-    estimated_energy_production = models.DecimalField(db_column='estimated_energy_production', max_digits=1000, decimal_places=4, blank=True)
+    #name = models.CharField(db_column='name', max_length=250)
+    deadline = models.DateTimeField(db_column='deadline', verbose_name="Határidő")
+    status = models.CharField(db_column='status', max_length=250, verbose_name="Státusz")
+    water_surface = models.CharField(db_column='water_surface', max_length=250, verbose_name="Vízfelület")
+    location = models.CharField(db_column='location', max_length=250, verbose_name="Helyszín")
+    flow_direction = models.CharField(db_column='flow_direction', max_length=250, choices=CARDINAL_DIRECTIONS_TUPLE, verbose_name="Áramlás iránya")
+    flow_direction_speed = models.FloatField(db_column='flow_direction_speed', max_length=250, verbose_name="Áramlás mért sebessége")
+    segment_width = models.DecimalField(db_column='segment_width', max_digits=1000, decimal_places=4, blank=True, verbose_name="Szelvény szélessége")
+    segment_depth = models.DecimalField(db_column='segment_depth', max_digits=1000, decimal_places=4, blank=True, verbose_name="Szelvény mélysége")
+    estimated_waterflow = models.CharField(db_column='estimated_waterflow', max_length=250, verbose_name="Becsült vízhozam")
+    estimated_energy_production = models.DecimalField(db_column='estimated_energy_production', max_digits=1000, decimal_places=4, blank=True, verbose_name="Becsült energia termelés")
     
     class Meta:
         db_table = 'hydrosurvey'
--- app/views/app.py
+++ app/views/app.py
@@ -14,6 +14,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.translation import ugettext as _
 from django import forms
 from webodm import settings
+from app.models.modelform import HydroSurveyForm
 
 def index(request):
     # Check first access
@@ -71,7 +72,23 @@ def tpfm_dashboard(request):
     return render(request, 'app/tpfm/tpfm_dashboard.html', {'title': _('Turbine planner & Fluid modelling'),'upload_success': False})
 
 @login_required
-def flow_simulation(request):
+def hydro_survey(request):
+    
+    if request.method == 'POST':
+        form = HydroSurveyForm(request.POST)
+        if form.is_valid():
+            # create a new `Survey` and save it to the db
+            survey = form.save()
+            # redirect to the detail page of the survey we just created
+            # we can provide the url pattern arguments as arguments to redirect function
+            return redirect('data_collection')
+    else:
+        form = HydroSurveyForm()
+
+    return render(request, 'app/psm/data_collection.html', {'form': form})
+
+@login_required
+def flow_simulation(request):  
     return render(request, 'app/tpfm/flow_simulation.html', {'title': _('Flow Simulation')})
 
 @login_required
