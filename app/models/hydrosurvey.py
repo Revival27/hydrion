bde9ccba3d64f8f5b26094f79cb16e96d76109ff
Andraspiti <andraspiti@gmail.com>
2024-04-23 11:26:10 +0200

    datepicker

--- app/models/hydrosurvey.py
+++ app/models/hydrosurvey.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 
 #CARDINAL_DIRECTIONS = ["Észak", "Kelet","Dél", "Nyugat"]
 #CARDINAL_DIRECTIONS_SORTED= sorted([(item, item) for item in CARDINAL_DIRECTIONS])
-CARDINAL_DIRECTIONS_TUPLE = (("É", "Észak"), ("K", "Kelet"), ("D", "Dél"), ("NY", "Nyugat"))
+CARDINAL_DIRECTIONS_TUPLE = (("N", "Észak"), ("E", "Kelet"), ("S", "Dél"), ("W", "Nyugat"))
 
 class HydroSurvey(models.Model):
     id = models.BigAutoField(db_column='id', primary_key=True)
@@ -15,7 +15,7 @@ class HydroSurvey(models.Model):
     status = models.CharField(db_column='status', max_length=250, verbose_name="Státusz")
     water_surface = models.CharField(db_column='water_surface', max_length=250, verbose_name="Vízfelület")
     location = models.CharField(db_column='location', max_length=250, verbose_name="Helyszín")
-    flow_direction = models.CharField(db_column='flow_direction', max_length=250, choices=CARDINAL_DIRECTIONS_TUPLE, verbose_name="Áramlás iránya")
+    flow_direction = models.CharField(db_column='flow_direction', max_length=10, choices=CARDINAL_DIRECTIONS_TUPLE, verbose_name="Áramlás iránya")
     flow_direction_speed = models.FloatField(db_column='flow_direction_speed', max_length=250, verbose_name="Áramlás mért sebessége")
     segment_width = models.DecimalField(db_column='segment_width', max_digits=1000, decimal_places=4, blank=True, verbose_name="Szelvény szélessége")
     segment_depth = models.DecimalField(db_column='segment_depth', max_digits=1000, decimal_places=4, blank=True, verbose_name="Szelvény mélysége")
--- app/models/modelform.py
+++ app/models/modelform.py
@@ -1,8 +1,17 @@
+from django import forms
 from django.forms import ModelForm
 from app.models.hydrosurvey import HydroSurvey
+
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 
 class HydroSurveyForm(ModelForm):
     class Meta:
         model = HydroSurvey
         fields = ['deadline', 'status', 'water_surface', 'location', 'flow_direction', 'flow_direction_speed', 'segment_width', 'segment_depth', 'estimated_waterflow', 'estimated_energy_production']
         #fields = ["Határidő", "Státusz", "Vízfelület", "Áramlás iránya", "Áramlás mért sebessége", "Szelvény szélessége", "Szelvény mélysége", "Becsült vízhozam", "Becsült energia termelés" ]
+        widgets = {
+            'deadline': DateInput(),
+        }
