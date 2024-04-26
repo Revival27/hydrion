eaa99a1b5a84e1139557a1ccebc4b2a7577f0ba3
nveronica <veronika_nagy@outlook.hu>
2024-04-26 12:58:11 +0200

    team member title added

--- app/models/team_member.py
+++ app/models/team_member.py
@@ -8,7 +8,8 @@ from django.utils.translation import gettext_lazy as _
 class TeamMember(models.Model):
     id = models.BigAutoField(db_column='id', primary_key=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    team = models.ForeignKey('Team', on_delete=models.CASCADE)   
+    team = models.ForeignKey('Team', on_delete=models.CASCADE)
+    title = models.CharField(db_column='title', max_length=250, null=True)
     def __str__(self):
         user = self.user.username
         return user
