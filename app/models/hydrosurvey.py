fff4f1a76fa72c598b105c56323337bb5cb45a80
nveronica <veronika_nagy@outlook.hu>
2024-05-17 12:14:46 +0200

    remove survey flow directions model

--- app/models/hydrosurvey.py
+++ app/models/hydrosurvey.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.postgres import fields
-from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 CARDINAL_DIRECTIONS_TUPLE = (("N", "Észak"), ("E", "Kelet"), ("S", "Dél"), ("W", "Nyugat"))
@@ -11,7 +9,7 @@ class HydroSurvey(models.Model):
     status = models.CharField(db_column='status', max_length=250, verbose_name="Státusz")
     water_surface = models.CharField(db_column='water_surface', max_length=250, verbose_name="Vízfelület")
     location = models.CharField(db_column='location', max_length=250, verbose_name="Helyszín")
-    flow_direction = models.ForeignKey('HydroSurveyFlowDirection', on_delete=models.CASCADE)
+    flow_direction = models.CharField(db_column='flow_direction', max_length=10, choices=CARDINAL_DIRECTIONS_TUPLE, verbose_name="Áramlás iránya")
     flow_direction_speed = models.FloatField(db_column='flow_direction_speed', max_length=250, verbose_name="Áramlás mért sebessége")
     segment_width = models.DecimalField(db_column='segment_width', max_digits=1000, decimal_places=4, blank=True, verbose_name="Szelvény szélessége")
     segment_depth = models.DecimalField(db_column='segment_depth', max_digits=1000, decimal_places=4, blank=True, verbose_name="Szelvény mélysége")
